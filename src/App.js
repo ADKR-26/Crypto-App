05a8b810f683f6710eaeb162ed5aa57042999caf
Aaditya <adityakumar@rubick.ai>
2023-03-01 15:00:12 +0530

    api added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [listOfCoins, setListOfCoins] = useState([]);
   
   useEffect(() => {
-    
-  });
+    console.log('useEffect');
+
+    axios.get('http://api.coinstats.app/public/v1/coins?skip=0&limit=10')
+    .then((response => {
+      const data = response.data.coins;
+      // console.log(data);
+      setListOfCoins(data);
+    }))
+
+  }, []);
 
   return (
     <div className="App">
@@ -15,7 +26,9 @@ function App() {
       </div>
 
       <div className='cryptoDisplay'>
-
+        {listOfCoins.map((coins) => {
+          return <h1> {coins.name} </h1>
+        })}
       </div>
 
     </div>
