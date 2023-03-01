acf6d75698713cf2ef789d4aba48b42b83b22a52
Aaditya <adityakumar@rubick.ai>
2023-03-01 16:50:31 +0530

    Basic layout Complete Full

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import CoinData from './components/coin';
 
 function App() {
 
   const [listOfCoins, setListOfCoins] = useState([]);
-  
+  const [searchValue, setSearchValue] = useState('');
+
   useEffect(() => {
     console.log('useEffect');
 
-    axios.get('http://api.coinstats.app/public/v1/coins?skip=0&limit=10')
-    .then((response => {
-      const data = response.data.coins;
-      // console.log(data);
-      setListOfCoins(data);
-    }))
+    axios.get('http://api.coinstats.app/public/v1/coins')
+      .then((response => {
+        const data = response.data.coins;
+        // console.log(data);
+        setListOfCoins(data);
+      }))
 
   }, []);
+
+  const filteredCoins = listOfCoins.filter((coins) => {
+    return coins.name.toLowerCase().includes(searchValue.toLowerCase());
+  });
 
   return (
     <div className="App">
 
       <div className='cryptoHeader'>
-
+        <h1> Crypto App </h1>
+        <input type="text" placeholder='Search Here' onChange={(event) => setSearchValue(event.target.value)}/>
       </div>
 
       <div className='cryptoDisplay'>
-        {listOfCoins.map((coins) => {
-          return <h1> {coins.name} </h1>
+
+        {filteredCoins.map((coins) => {
+          return <CoinData
+            name={coins.name}
+            icon={coins.icon}
+            price={coins.price}
+            symbol={coins.symbol}
+          />
         })}
       </div>
 
