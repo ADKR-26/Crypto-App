a72e1f81510c77f578c290db874d21685d566fed
Aaditya Kumar <adityavishwakarma2011@gmail.com>
2023-09-22 22:11:22 +0530

    Added Firebase Analytics

--- src/App.js
+++ src/App.js
@@ -5,20 +5,43 @@ import CoinData from './components/coin';
 import VisitCount from './components/VisitCount';
 import DeviceData from './components/DeviceData';
 
-function App() {
+// Import Firebase and initialize it with your Firebase config
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/analytics';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDL5dEGpcSC6VFpZ_lJa20EfTFWQUi-35o",
+  authDomain: "portfolio-174c9.firebaseapp.com",
+  projectId: "portfolio-174c9",
+  storageBucket: "portfolio-174c9.appspot.com",
+  messagingSenderId: "882529091555",
+  appId: "1:882529091555:web:84d2aef6f05c253de22ad6",
+  measurementId: "G-07LGR4W416"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics();
+
+function App() {
   const [listOfCoins, setListOfCoins] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
-  useEffect(() => {
-    // console.log('useEffect');
+  console.log("TESTT", analytics);
+  
 
+  useEffect(() => {
     axios.get('https://api.coinstats.app/public/v1/coins')
       .then((response => {
         const data = response.data.coins;
         setListOfCoins(data);
       }))
+      .catch(error => {
+        console.error('Error fetching cryptocurrency data:', error);
+      });
 
+    // Log an analytics event when the component mounts
+    firebase.analytics().logEvent('app_loaded');
   }, []);
 
   const filteredCoins = listOfCoins.filter((coins) => {
@@ -27,23 +50,26 @@ function App() {
 
   return (
     <div className="App">
-        <DeviceData />
+      <DeviceData />
 
       <div className='cryptoHeader'>
         <h1> Crypto App </h1>
         <input type="text" placeholder='Search Here' onChange={(event) => setSearchValue(event.target.value)}/>
       </div>
-        <VisitCount />
+
+      <VisitCount />
 
       <div className='cryptoDisplay'>
-
         {filteredCoins.map((coins) => {
-          return <CoinData
-            name={coins.name}
-            icon={coins.icon}
-            price={coins.price}
-            symbol={coins.symbol}
-          />
+          return (
+            <CoinData
+              key={coins.id} // Add a unique key for each coin
+              name={coins.name}
+              icon={coins.icon}
+              price={coins.price}
+              symbol={coins.symbol}
+            />
+          );
         })}
       </div>
     </div>
